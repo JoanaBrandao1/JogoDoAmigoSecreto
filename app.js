ef6b0e9d704a2a7955b93c7d3ce89ad247ce4aa8
JoanaBrandao1 <joana.beatriz@ufrpe.br>
2025-03-16 05:01:51 -0300

    problema proposto resolvido, organização no proximo commit

--- app.js
+++ app.js
@@ -1,30 +1,52 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos=[];
 let listaAmigos=document.getElementById('listaAmigos');
-
+let amigosSorteados=[];
 
 function adicionarAmigo(){
     let nome= document.getElementById('amigo').value;
     verificarExistencia(nome);
     if(nome==""){
-        alert('Digite um nome válido');
+        alert('Por favor, insira um nome');
     }else{
         listaAmigos.innerHTML  = amigos;
     }
-    limparCampo();
+    limparCampo('amigo');
 }
 function verificarExistencia(nome){
     if(amigos.includes(nome)){
-        alert('Este nome já foi digitado, digite outro nome');
+        alert('O nome já foi inserido, por favor insira outro');
     }else{
-        amigos.push(nome)
+        amigos.push(nome);
     }
 }
-function gerarUmNomeAleatorio(){
-    let numeroAleatorio=parseInt(Math.random() * numeroLimte + 1);
+amigos.forEach(function(amigo) {
+    const li = document.createElement("li");
 
+    li.textContent = amigo;
+
+    lista.appendChild(li);
+});
+
+if(amigoSorteado==""){resultado.innerHTML="";}
+
+sortearAmigo();
+
+
+function sortearAmigo(){
+    
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+    let resultado=document.getElementById('resultado');
+    
+    if(amigosSorteados.includes(amigoSorteado)){
+        return sortearAmigo();
+    }else{
+        amigosSorteados.push(amigoSorteado)
+        resultado.innerHTML = amigoSorteado;
+    }
 }
-function limparCampo(){
-    campo=document.getElementById('amigo');
+function limparCampo(tipo){
+    campo=document.getElementById(tipo);
     campo.value='';
 }
